96b11009d30c8387a1a45b4230f2dbffe0a21cbb
ml-andy <andy2472625@gmail.com>
2018-01-27 11:10:15 +0800

    hotfix

--- src/js/store/modules/recommend.js
+++ src/js/store/modules/recommend.js
@@ -98,29 +98,23 @@ const recommend = {
           console.log("error:", xhr, textStatus, errorThrown);
           commit('catchPostError', errorThrown, { root: true });
 				}
-			}); 
-      // axios.post(`${rootState.backEndUrl}/teller_reference`, {
-      //     params: {
-      //       teller_id: rootState.teller_id,
-      //       customer_id: rootState.customer_id,
-      //       token: rootState.token,
-      //       recommendation: {
-      //         [key]: type
-      //       }
-      //     }
-      //   })
-      //   .then(({data})=>{
-      //     if (data.api_code !== 'CustomerJourney_0000'){
-      //       commit('catchPostError', data, { root: true });
-      //       return
-      //     }
+      }); 
+      
+      let postDataQs = Qs.stringify(datas);
+      axios.post(`${rootState.backEndUrl}/teller_reference`, postDataQs)
+        .then(({data})=>{
+          console.log('axios');
+          if (data.api_code !== 'CustomerJourney_0000'){
+            commit('catchPostError', data, { root: true });
+            return
+          }
           
-      //     console.log(data);
-      //     type ? state[key] = 1 : state[key] = -1
-      //   })
-      //   .catch(err => {
-      //     commit('catchPostError', err, { root: true });
-      //   })
+          console.log(data);
+          type ? state[key] = 1 : state[key] = -1
+        })
+        .catch(err => {
+          commit('catchPostError', err, { root: true });
+        })
     },
   }
 }
